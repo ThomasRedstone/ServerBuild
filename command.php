1b4f304c7ceceae463b8be6a8eda3658f31e9e5a
Thomas Redstone <tom@redstonetechnology.net>
2016-05-21 21:50:01 +0100

    Refactoring.

--- command.php
+++ command.php
@@ -1,7 +1,5 @@
 #!/usr/bin/env php
 <?php
 // application.php
-
-define('APP_PATH', __DIR__);
 require APP_PATH.'/bootstrap.php';
 require APP_PATH.'/vendor/autoload.php';
---
+++ start.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomasredstone
+ * Date: 21/05/2016
+ * Time: 21:49
+ */
+define('APP_PATH', __DIR__);
