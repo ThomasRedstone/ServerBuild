4974db8a58dfe8fff5cc9481cbad122f856a73f6
Thomas Redstone <tom@redstonetechnology.net>
2016-05-21 21:21:09 +0100

    Added bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomasredstone
+ * Date: 21/05/2016
+ * Time: 21:20
+ */
+
+define('APP_PATH', __DIR__);
+require __DIR__.'/vendor/autoload.php';
+
+use RedstoneTechnology\ServerBuild\Commands;
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+$application->add(
+    new Commands\ServerBuild(
+        new \RedstoneTechnology\ServerBuild\Utilities\ServerBuild(
+            new \RedstoneTechnology\ServerBuild\Utilities\Packages()
+        )
+    )
+);
+$application->run();
--- command.php
+++ command.php
@@ -2,19 +2,4 @@
 <?php
 // application.php
 
-define('APP_PATH', __DIR__);
-
-require __DIR__.'/vendor/autoload.php';
-
-use RedstoneTechnology\ServerBuild\Commands;
-use Symfony\Component\Console\Application;
-
-$application = new Application();
-$application->add(
-    new Commands\ServerBuild(
-        new \RedstoneTechnology\ServerBuild\Utilities\ServerBuild(
-            new \RedstoneTechnology\ServerBuild\Utilities\Packages()
-        )
-    )
-);
-$application->run();
+require 'bootstrap.php';
